c439792f6c26642d918bcf2abeed4bc4cda989e9
Jamie <Jay.mudle@gmail.com>
2023-10-15 02:33:18 +0100

    JM added limited CSV functionality

--- carbcalculator.py
+++ carbcalculator.py
@@ -1,24 +1,46 @@
+import csv
+
 #def CarbCalculator function
 
 def CarbCalculator():
     
     #Setting vars for carb input
     try:
+        product = input("Enter product:")
         x = input("Enter Listed Carbs:")
         y = input("Enter Product Weight:")
         z = input("Enter Portion Size:")
         
         #Setting vars for carb calculation
-
+        productvalue = str(product)
         xx = float(x)
         yy = float(y)
         zz = float(z)
         
         #Carb Calculator
-
+        
         result = round(xx / yy * zz, 1)
+        header = ["product, Portion Size, Carbohydates, Carbs Per Portion"]
+        data = f"{productvalue}, {xx}, {zz}, {result}"
         print(result)
+
+        with open('carbs_and_portions.csv', 'w') as f:
+            headerwriter = csv.writer(f)
+
+            headerwriter.writerow([header])
+
+            return(True)
+
+        
+        with open('carbs_and_portions.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
+            
+            writer.writerow([data])
+            
+            return(True)
+        
     #Error Handling
-    except:
+    except Exception:
         print("Error:Please enter a numerical value")
+        
 CarbCalculator()
